b81cd572fb286d39e09514efacda487e474d41c4
sonika2608 <93131984+sonika2608@users.noreply.github.com>
2024-07-08 00:44:29 +0530

    Update global-setup.ts

--- global-setup.ts
+++ global-setup.ts
@@ -1,26 +1,21 @@
 import { chromium, type FullConfig } from '@playwright/test';
 
 async function globalSetup(config: FullConfig) {
-//   const { baseURL } = config.projects[0].use;
-//   const browser = await chromium.launch({headless:false});
-//   const page = await browser.newPage();
-//   await page.goto(baseURL!);
-//   await page.locator('#nav-tools a[data-nav-role="signin"]').hover();
-// //   await page.locator('#nav-flyout-ya-signin .nav-action-inner').click();
-// //   await page.getByLabel('User Name').fill('user');
-// //   await page.getByLabel('Password').fill('password');
-// //   await page.getByText('Sign in').click();
-
-//   await page.waitForTimeout(1000);
-//   await page.getByRole('link', { name: 'Sign in', exact: true }).click();
-//   await page.waitForTimeout(1000);
-//   await page.getByLabel('Enter your mobile number or').fill(process.env.USERNAME!);
-//   await page.getByLabel('Continue').click();
-//   await page.getByLabel('Password').fill(process.env.PASSWORD!);
-//   await page.getByLabel('Sign in').click();
-//   await page.waitForTimeout(6000);
-//   await page.context().storageState({ path: 'storage-state.json' });
-//   await browser.close();
+  const { baseURL } = config.projects[0].use;
+  const browser = await chromium.launch({headless:false});
+  const page = await browser.newPage();
+  await page.goto(baseURL!);
+  await page.locator('#nav-tools a[data-nav-role="signin"]').hover();
+  await page.waitForTimeout(1000);
+  await page.getByRole('link', { name: 'Sign in', exact: true }).click();
+  await page.waitForTimeout(1000);
+  await page.getByLabel('Enter your mobile number or').fill(process.env.USERNAME!);
+  await page.getByLabel('Continue').click();
+  await page.getByLabel('Password').fill(process.env.PASSWORD!);
+  await page.getByLabel('Sign in').click();
+  await page.waitForTimeout(6000);
+  await page.context().storageState({ path: 'storage-state.json' });
+  await browser.close();
 }
 
 export default globalSetup;
