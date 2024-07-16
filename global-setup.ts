5a754fb67a1e0ba43575035e7b6aec659e5c3ad4
sonika2608 <93131984+sonika2608@users.noreply.github.com>
2024-07-16 20:26:27 +0530

    Update global-setup.ts

--- global-setup.ts
+++ global-setup.ts
@@ -6,15 +6,16 @@ async function globalSetup(config: FullConfig) {
   const page = await browser.newPage();
   await page.goto(baseURL!);
   await page.locator('#nav-tools a[data-nav-role="signin"]').hover();
-  await page.waitForTimeout(1000);
   await page.getByRole('link', { name: 'Sign in', exact: true }).click();
-  await page.waitForTimeout(1000);
   await page.getByLabel('Enter your mobile number or').fill(process.env.USERID!);
   await page.getByLabel('Continue').click();
   await page.getByLabel('Password').fill(process.env.PASSWORD!);
   await page.getByLabel('Sign in').click();
-  await page.waitForTimeout(6000);
   await page.context().storageState({ path: 'storage-state.json' });
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Amazon.in/);
+  // Expect page should not contain text "sign in".
+  await expect(page.locator('#nav-link-accountList-nav-line-1')).not.toContainText(/sign in/);
   await browser.close();
 }
 
