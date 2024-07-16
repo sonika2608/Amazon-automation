147e0a694360f68a7703281d61fc951949da7f48
sonika2608 <93131984+sonika2608@users.noreply.github.com>
2024-07-16 20:29:39 +0530

    Update gotoHome.page.ts

--- pages/gotoHome.page.ts
+++ pages/gotoHome.page.ts
@@ -2,12 +2,9 @@ import { Locator, type Page } from '@playwright/test';
 
 export class GotoHomePage {
   readonly page: Page;
-  readonly accountName: Locator;
-
 
   constructor(page: Page) {
     this.page = page;
-    this.accountName = page.locator('#nav-link-accountList-nav-line-1')
   }
 
   async gotoAmazon() {
